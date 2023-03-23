b94b2df709b9823823c44676e14a2b915f6a0311
Mike-droid <miguelreyesmoreno@hotmail.com>
2023-03-22 18:45:05 -0600

    search bar and country dropdown

--- src/pages/boxers/index.js
+++ src/pages/boxers/index.js
@@ -1,13 +1,55 @@
+import React, { useState, useEffect } from 'react';
 import BoxerCard from '@/components/boxerCard'
 import Navbar from '@/components/navbar'
+import { MyFooter } from '@/components/myFooter'
 import { apiUrl } from '@/api/apiUrl'
 
-export default function boxers({ data }) {
+export default function Boxers({ data }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    setFilteredData(
+      data.filter(boxer =>
+        boxer.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (selectedCountry === '' || boxer.country === selectedCountry)
+      )
+    );
+  }, [searchTerm, selectedCountry, data]);
+
+  const handleInputChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCountryChange = event => {
+    setSelectedCountry(event.target.value);
+  };
+
   return(
-    <>
+    <div className='h-screen'>
       <Navbar/>
-      <section className='flex flex-wrap flex-col content-center md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 2xl:grid 2xl:grid-cols-4 gap-5 justify-items-center'>
-        {data.map((boxer) => (
+      <div className='flex flex-col items-center md:flex-row md:justify-around'>
+        <input
+          type='search'
+          placeholder='Search boxers by name'
+          value={searchTerm}
+          onChange={handleInputChange}
+          className='p-2'
+        />
+        <div>
+          <label htmlFor='country-select'>Filter by country:</label>
+          <select id='country-select' value={selectedCountry} onChange={handleCountryChange}>
+            <option value=''>All</option>
+            <option value='Mexico'>Mexico</option>
+            <option value='Japan'>Japan</option>
+            <option value='USA'>USA</option>
+            <option value='Russia'>Russia</option>
+          </select>
+        </div>
+      </div>
+      <section className='mb-auto pb-64 flex flex-wrap flex-col content-center md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 2xl:grid 2xl:grid-cols-4 gap-5 justify-items-center'>
+        {filteredData.map((boxer) => (
           <BoxerCard
             key={boxer.id}
             name={boxer.name}
@@ -22,7 +64,8 @@ export default function boxers({ data }) {
           />
         ))}
       </section>
-    </>
+      <MyFooter/>
+    </div>
   )
 }
 
