7652d262953fffcb87e854e854cecae897e04044
Mike-droid <miguelreyesmoreno@hotmail.com>
2023-03-23 12:02:52 -0600

    styles for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,13 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <h1 className='text-3xl font-bold'>
-        Hajime no ippo!
+      <h1 className='text-3xl sm:text-9xl font-bold text-center mt-4'>
+        <span className='text-blue-500'>
+          Hajime no
+        </span>
+        <span className='text-red-500'>
+          ippo!
+        </span>
       </h1>
       <MyFooter/>
     </>
