61c08f39a647ab7905b7ee8f3fba2159065660b8
Mike-droid <miguelreyesmoreno@hotmail.com>
2023-03-22 19:17:02 -0600

    styles for footer

--- src/components/myFooter.js
+++ src/components/myFooter.js
@@ -1,16 +1,19 @@
-import Image from "next/image"
+import Image from 'next/image'
 import githubImage from '../images/GitHub-Mark.png'
+import styles from '../styles/darkPurple.module.css'
 
 export const MyFooter = () => {
   return (
-    <footer>
-      <a href='https://github.com/Mike-droid' target='_blank'>Made by: Mike-droid</a>
-      <a href='https://github.com/Mike-droid/hajime-no-ippo-nextjs'>
-        <Image priority src={githubImage} alt='Github icon' width={100} height={100}/>
+    <footer className={`absolute bottom-0 flex flex-col sm:flex-row justify-around w-full ${styles.darkPurple}`}>
+      <a className='text-cyan-400 text-center' href='https://github.com/Mike-droid' target='_blank'>
+        Made by: Mike-droid
+      </a>
+      <a className='text-cyan-400 flex flex-row justify-center' href='https://github.com/Mike-droid/hajime-no-ippo-nextjs'>
+        <Image priority src={githubImage} alt='Github icon' width={30} height={30} className='invert'/>
         Github repository of this project
       </a>
-      <a href='https://github.com/Mike-droid/hajime-no-ippo-api'>
-        <Image priority src={githubImage} alt='Github icon' width={100} height={100}/>
+      <a className='text-cyan-400 flex flex-row justify-center' href='https://github.com/Mike-droid/hajime-no-ippo-api'>
+        <Image priority src={githubImage} alt='Github icon' width={30} height={30} className='invert'/>
         Github repository of the API
       </a>
     </footer>
