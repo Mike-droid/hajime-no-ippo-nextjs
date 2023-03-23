abadd217676ed7fcd655285c15e58797a8e57bff
Mike-droid <miguelreyesmoreno@hotmail.com>
2023-03-22 19:53:28 -0600

    styles on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-800 flex flex-row mb-3 h-14 text-xl'>
-      <Link href='/' className='flex-auto'>
+    <nav className='bg-gray-800 flex flex-wrap content-center flex-row mb-3 h-14 text-xl'>
+      <Link href='/' className='flex-auto ml-2'>
         Home
       </Link>
-      <Link href='/boxers' className='flex-auto'>
+      <Link href='/boxers' className='flex-auto ml-2'>
         Boxers
       </Link>
     </nav>
